4b061a973121c145510788a77e174e551c8c6404
Genbu Hase <hasegenbu@gmail.com>
2018-08-14 21:46:44 +0900

    大幅な改善・追加・実装

--- js/voez.js
+++ js/voez.js
@@ -1,6 +1,25 @@
-function debug(hoge) {
-    alert(hoge);
-    console.log(hoge);
-    var ass = 0;
+const voezTabsCollection = document.querySelectorAll(".voez-tabs");
+
+
+
+for (const voezTabs of voezTabsCollection) {
+	const cover = voezTabs.querySelector(".voez-tabs--cover");
+	const holder = voezTabs.querySelector(".voez-tabs--holder");
+
+	holder.classList.add("hidden");
+
+	while (holder.children.length < 4) {
+		const tab = document.createElement("div");
+		tab.classList.add("voez-tab");
+
+		const tabContent = document.createElement("span");
+		tabContent.classList.add("voez-tile--item");
+
+		tab.appendChild(tabContent);
+		holder.insertBefore(tab, holder.querySelector(".voez-tab.voez-tab--back"));
+	}
+
+	voezTabs.addEventListener("click", () => {
+		cover.classList.toggle("hidden");
+	});
 }
-//# sourceMappingURL=voez.js.map
